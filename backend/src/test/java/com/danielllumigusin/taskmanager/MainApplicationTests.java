13d079ff81a4fd50b2149358551f3c9204cfd3fc
Daniel Gonzalo <118071772+DanielLlumigusin@users.noreply.github.com>
2025-05-10 17:22:00 -0500

    Initialize Spring Boot project

---
+++ backend/src/test/java/com/danielllumigusin/taskmanager/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielllumigusin.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
